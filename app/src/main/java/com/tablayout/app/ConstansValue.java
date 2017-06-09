ba4b156e1edd7b6dcdd45a8080b1e3ece20aa888
weishl110 <weishl110@163.com>
2017-06-09 09:57:08 +0800

    Initial commit

---
+++ app/src/main/java/com/tablayout/app/ConstansValue.java
@@ -0,0 +1,22 @@
+package com.tablayout.app;
+
+/**
+ * Created by ${wei} on 2017/6/6.
+ */
+
+public class ConstansValue {
+
+    public static final int[] bueatyResIds = {R.mipmap.beauty1,R.mipmap.beauty2,R.mipmap.beauty3,
+            R.mipmap.beauty4,R.mipmap.beauty5,R.mipmap.beauty6,
+            R.mipmap.beauty7,R.mipmap.beauty8,R.mipmap.beauty9,
+            R.mipmap.beauty10,R.mipmap.beauty11,R.mipmap.beauty12,
+            R.mipmap.beauty13};
+    public static final int[] sceneryResIds = {R.mipmap.scenery14,R.mipmap.scenery15,
+            R.mipmap.scenery16, R.mipmap.scenery17,R.mipmap.scenery18,R.mipmap.scenery19,
+            R.mipmap.scenery20, R.mipmap.scenery21,R.mipmap.scenery22,R.mipmap.scenery23,
+            R.mipmap.scenery24, R.mipmap.scenery25,R.mipmap.scenery26};
+    public static final int[] carResIds = {R.mipmap.car1,R.mipmap.car2,R.mipmap.car3,R.mipmap.car4,
+            R.mipmap.car5,R.mipmap.car6,R.mipmap.car7,R.mipmap.car8,R.mipmap.car9,R.mipmap.car10,
+            R.mipmap.car11,R.mipmap.car12,R.mipmap.car13,R.mipmap.car14,R.mipmap.car15,R.mipmap.car16,
+            R.mipmap.car17, R.mipmap.car18,R.mipmap.car19,R.mipmap.car20};
+}
---
+++ app/src/main/java/com/tablayout/app/OneFragment.java
@@ -0,0 +1,25 @@
+package com.tablayout.app;
+
+import android.view.View;
+
+/**
+ * Created by ${wei} on 2017/5/31.
+ */
+
+public class OneFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_layout_one;
+    }
+
+    @Override
+    protected void initView(View rootView) {
+    }
+
+    @Override
+    protected void initDate() {
+
+    }
+
+}
